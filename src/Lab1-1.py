8211e2db33754e8e4baa757b1db5f82a06bb6b92
Cjochiwon <101166118+Cjochiwon@users.noreply.github.com>
2025-10-17 16:54:48 +0900

    Fix selection sort comparison operator and update prints

--- src/Lab1-1.py
+++ src/Lab1-1.py
@@ -1,11 +1,12 @@
 #Lab1-1 selection sorting (최소 값 중심)
+#코드를 수정하였습니다.
 
 def selection_sort(data_list):
     for i in range(len(data_list) - 1):
         min_index = i
 
         for j in range(i + 1, len(data_list)):
-            if data_list[j] < data_list[min_index]:
+            if data_list[j] > data_list[min_index]:
                 min_index = j
 
         if min_index != i:
@@ -14,7 +15,7 @@ def selection_sort(data_list):
     return data_list
 
 data = [64, 25, 12, 22, 11]
-print("정렬 전 리스트: %s" % data)
+print("처음 리스트: %s" % data)
 
 sorted_data = selection_sort(data)
 print("정렬 후 리스트: %s " % sorted_data)
